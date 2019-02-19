6b47babfbcaad6e54a9a3a16b934944e4cad38c5
Robert Gunderson rdgunderson@bsu.edu <rdgunderson@bsu.edu>
2019-02-19 01:01:47 -0500

    Added functionality to take geojson file weather data and apply to map

--- app/src/main/java/edu/bsu/rdgunderson/armchairstormchaserapp/MainActivity.java
+++ app/src/main/java/edu/bsu/rdgunderson/armchairstormchaserapp/MainActivity.java
@@ -1,8 +1,11 @@
 package edu.bsu.rdgunderson.armchairstormchaserapp;
 
+import android.app.Activity;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.mapbox.api.geocoding.v5.models.CarmenFeature;
 import com.mapbox.mapboxsdk.Mapbox;
 //import com.mapbox.mapboxsdk.constants.Style;
 import com.mapbox.mapboxsdk.camera.CameraPosition;
@@ -27,26 +30,115 @@ import java.util.ArrayList;
 import java.util.List;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.widget.Toast;
+
 import edu.bsu.rdgunderson.armchairstormchaserapp.R;
 
-public class MainActivity extends AppCompatActivity {
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+
+//import com.mapbox.mapboxandroiddemo.R;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
+import com.mapbox.mapboxsdk.style.layers.CircleLayer;
+import com.mapbox.mapboxsdk.style.layers.FillLayer;
+import com.mapbox.mapboxsdk.style.layers.PropertyFactory;
+import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import static com.mapbox.mapboxsdk.style.expressions.Expression.eq;
+import static com.mapbox.mapboxsdk.style.expressions.Expression.literal;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private MapView mapView;
-    private static final int PLACE_SELECTION_REQUEST_CODE = 56789;
+    /*private static final int PLACE_SELECTION_REQUEST_CODE = 56789;
     private static final String MARKER_SOURCE = "markers-source";
     private static final String MARKER_STYLE_LAYER = "markers-style-layer";
-    private static final String MARKER_IMAGE = "custom-marker";
+    private static final String MARKER_IMAGE = "custom-marker";*/
+    private static final String mapboxKey = "pk.eyJ1Ijoic3RyaXBlZHdyaXN0YmFuZHMiLCJhIjoiY2pvN3VrYWx6MDJsZjN3dGt1bDNjd2c0aiJ9.qeI4-uMxyL5JnEiPi3UVSQ";
 
+
+    private static final String GEOJSON_SOURCE_ID = "GEOJSONFILE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        String mapboxKey = "pk.eyJ1Ijoic3RyaXBlZHdyaXN0YmFuZHMiLCJhIjoiY2pvN3VrYWx6MDJsZjN3dGt1bDNjd2c0aiJ9.qeI4-uMxyL5JnEiPi3UVSQ";
+
+        Mapbox.getInstance(this, mapboxKey);
+
+        setContentView(R.layout.activity_main);
+
+        mapView = findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull MapboxMap mapboxMap) {
+            mapboxMap.setStyle(new Style.Builder().fromUrl("mapbox://styles/stripedwristbands/cjrs8ad75iwpe2so1sq19ayom"), new Style.OnStyleLoaded() {
+
+                @Override
+            public void onStyleLoaded(@NonNull Style style) {
+                createGeoJsonSource(style);
+                addPolygonLayer(style);
+                addPointsLayer(style);
+            }
+        });
+    }
+
+    private void createGeoJsonSource(@NonNull Style loadedMapStyle) {
+        loadedMapStyle.addSource(new GeoJsonSource(GEOJSON_SOURCE_ID,
+                loadJsonFromAsset("Tornado_Watch.geojson")));
+    }
+
+    private void addPolygonLayer(@NonNull Style loadedMapStyle) {
+        FillLayer countryPolygonFillLayer = new FillLayer("polygon", GEOJSON_SOURCE_ID);
+        countryPolygonFillLayer.setProperties(
+                PropertyFactory.fillColor(Color.RED),
+                PropertyFactory.fillOpacity(.4f));
+        countryPolygonFillLayer.setFilter(eq(literal("$type"), literal("Polygon")));
+        loadedMapStyle.addLayer(countryPolygonFillLayer);
+    }
+
+    private void addPointsLayer(@NonNull Style loadedMapStyle) {
+        CircleLayer individualCirclesLayer = new CircleLayer("points", GEOJSON_SOURCE_ID);
+        individualCirclesLayer.setProperties(
+                PropertyFactory.circleColor(Color.YELLOW),
+                PropertyFactory.circleRadius(3f));
+        individualCirclesLayer.setFilter(eq(literal("$type"), literal("Point")));
+        loadedMapStyle.addLayer(individualCirclesLayer);
+    }
+
+    private String loadJsonFromAsset(String filename) {
+        try {
+            InputStream is = getAssets().open(filename);
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            return new String(buffer, "UTF-8");
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
+    /*@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         Mapbox.getInstance(this, mapboxKey);
         setContentView(R.layout.activity_main);
         mapView = findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
-//        mapView.setStyleUrl("mapbox://styles/stripedwristbands/cjrs8ad75iwpe2so1sq19ayom");
 
 
         mapView.getMapAsync(new OnMapReadyCallback() {
@@ -61,13 +153,14 @@ public class MainActivity extends AppCompatActivity {
                         // Map is set up and the style has loaded. Now you can add data or make other map adjustments
 
 
+
                     }
                 });
             }
         });
-    }
+    }*/
 
-    //Mapbox code to create a marker
+
     /*@Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -75,23 +168,23 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-        *//* Map: This represents the map in the application. *//*
         mapView = findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
-        mapView.getMapAsync(this);
-    }
+        mapView.getMapAsync((OnMapReadyCallback) this);
 
-    @Override
-    public void onMapReady(@NonNull final MapboxMap mapboxMap) {
-        mapboxMap.setStyle(new Style.Builder().fromUrl(mapbox://styles/mapbox/light-v10), new Style.OnStyleLoaded() {
-        @Override
-        public void onStyleLoaded(@NonNull Style style) {
-            *//* Image: An image is loaded and added to the map. *//*
-            style.addImage(MARKER_IMAGE, BitmapFactory.decodeResource(
-                    MainActivity.this.getResources(), R.drawable.custom_marker));
-            addMarkers(style);
-        }
-    });
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull final MapboxMap mapboxMap) {
+                mapboxMap.setStyle(new Style.Builder().fromUrl("mapbox://styles/mapbox/light-v10"), new Style.OnStyleLoaded() {
+                    @Override
+                    public void onStyleLoaded(@NonNull Style style) {
+                        style.addImage(MARKER_IMAGE, BitmapFactory.decodeResource(
+                                MainActivity.this.getResources(), R.drawable.custom_marker));
+                        addMarkers(style);
+                    }
+                });
+            }
+        });
 }
 
     private void addMarkers(@NonNull Style loadedMapStyle) {
