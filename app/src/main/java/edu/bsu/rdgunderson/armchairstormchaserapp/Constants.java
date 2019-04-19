0179591abedd2149578a65d664084adcde22f37e
Robert <rdgunderson@bsu.edu>
2019-04-19 16:17:47 -0400

    Cleaned a few things up, and removed action bar

--- app/src/main/java/edu/bsu/rdgunderson/armchairstormchaserapp/Constants.java
+++ app/src/main/java/edu/bsu/rdgunderson/armchairstormchaserapp/Constants.java
@@ -5,5 +5,6 @@ public class Constants {
     public static final String MAPBOX_API_KEY = "pk.eyJ1Ijoic3RyaXBlZHdyaXN0YmFuZHMiLCJhIjoiY2pvN3VrYWx6MDJsZjN3dGt1bDNjd2c0aiJ9.qeI4-uMxyL5JnEiPi3UVSQ";
 //    public static final String MAPBOX_STYLE_URL = "mapbox://styles/stripedwristbands/cjrs8ad75iwpe2so1sq19ayom";
 //    public static final String MAPBOX_STYLE_URL = "mapbox://styles/stripedwristbands/cjojc7pow0bjv2st5rkoinjza";
-    public static final String MAPBOX_STYLE_URL = "mapbox://styles/stripedwristbands/cju4n1qm501cp1gqmr817fpuw";
+    public static final String MAPBOX_STYLE_URL = "mapbox://styles/stripedwristbands/cju0hv17t0e4t1fmci3tbgihm";
+    public static final int REFRESH_RATE_IN_SECONDS = 5;
 }
--- app/src/main/java/edu/bsu/rdgunderson/armchairstormchaserapp/MainActivity.java
+++ app/src/main/java/edu/bsu/rdgunderson/armchairstormchaserapp/MainActivity.java
@@ -1,5 +1,24 @@
 package edu.bsu.rdgunderson.armchairstormchaserapp;
 
+import timber.log.Timber;
+
+import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Timer;
+import java.util.TimerTask;
+import static java.lang.Math.floor;
+
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -31,21 +50,6 @@ import com.mapbox.mapboxsdk.style.layers.Property;
 import com.mapbox.mapboxsdk.style.layers.PropertyFactory;
 import com.mapbox.mapboxsdk.style.layers.SymbolLayer;
 import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Timer;
-import java.util.TimerTask;
-
-import io.socket.client.Socket;
-import io.socket.emitter.Emitter;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import timber.log.Timber;
 import static com.mapbox.core.constants.Constants.PRECISION_6;
 import static com.mapbox.mapboxsdk.style.expressions.Expression.eq;
 import static com.mapbox.mapboxsdk.style.expressions.Expression.literal;
@@ -57,7 +61,6 @@ import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.lineCap;
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.lineColor;
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.lineJoin;
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.lineWidth;
-import static java.lang.Math.floor;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -69,7 +72,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     private double destinationLongitute;
 
     private static final String GEOJSON_SOURCE_ID = "GEOJSONFILE";
-//    private static final String GEOJSON_SOURCE_ID = "geojson-source";
 
     private static final String ROUTE_LAYER_ID = "route-layer-id";
     private static final String ROUTE_SOURCE_ID = "route-source-id";
@@ -80,18 +82,17 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     private MapboxDirections client;
     private Point origin;
     private Point destination;
-
-    private boolean isMarkers = false;
-//    private boolean isWeather = false;
-    private boolean isDestinationMarkers = false;
     private SymbolLayer originMarkerSymbolLayer = null;
     private GeoJsonSource originMarkerGeoJsonSource = null;
     final Handler handler = new Handler();
     Timer timer;
     TimerTask timerTask;
-    private boolean hasSetRoute = false;
-
     private View login;
+
+    private boolean isMarkers = false;
+//    private boolean isWeather = false;
+    private boolean isDestinationMarkers = false;
+    private boolean hasSetRoute = false;
 
     public Point currentLocation = Point.fromLngLat(currentLongitute, currentLattitude);
 
@@ -101,67 +102,55 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Mapbox.getInstance(this, Constants.MAPBOX_API_KEY);
-
         setContentView(R.layout.activity_main);
-
         mapView = findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(this);
-
     }
 
     @Override
     public void onMapReady(@NonNull final MapboxMap mapboxMap) {
         mapboxMap.setStyle(new Style.Builder().fromUrl(Constants.MAPBOX_STYLE_URL), new Style.OnStyleLoaded() {
-        //mapboxMap.setStyle(Style.LIGHT, new Style.OnStyleLoaded() {
-                @Override
+            @Override
             public void onStyleLoaded(@NonNull Style style) {
                 createGeoJsonSource(style);
                 addPolygonLayer(style);
+                ArmchairStormChaser app = (ArmchairStormChaser)getApplication();
+                socket = app.getSocket();
 
-                    ArmchairStormChaser app = (ArmchairStormChaser)getApplication();
-                    socket = app.getSocket();
+                // socket.on(Socket.EVENT_CONNECT, onConnect);
+                // socket.on(Socket.EVENT_DISCONNECT, onDisconnect);
+                // socket.on(Socket.EVENT_CONNECT_ERROR, onConnectError);
+                // socket.on(Socket.EVENT_CONNECT_TIMEOUT, onConnectError);
+                socket.on("updatePlayer", onUpdatePlayer);
+                socket.connect();
 
-                    // socket.on(Socket.EVENT_CONNECT, onConnect);
-                    // socket.on(Socket.EVENT_DISCONNECT, onDisconnect);
-                    // socket.on(Socket.EVENT_CONNECT_ERROR, onConnectError);
-                    // socket.on(Socket.EVENT_CONNECT_TIMEOUT, onConnectError);
-                    socket.on("updatePlayer", onUpdatePlayer);
-                    socket.connect();
+                //Add Current Position Icon on App start
+                style.addImage("origin-marker-icon-id",
+                        BitmapFactory.decodeResource(
+                                MainActivity.this.getResources(), R.drawable.custom_marker));
 
-                //setContentView(R.layout.mapbox_view_search);
+                //Retrieve current location from server
+                originMarkerGeoJsonSource = new GeoJsonSource("origin-source-id", Feature.fromGeometry(
+                        Point.fromLngLat(currentLongitute, currentLattitude)));
+                style.addSource(originMarkerGeoJsonSource);
 
-                    //Add Current Position Icon on App start
-                    style.addImage("origin-marker-icon-id",
-                            BitmapFactory.decodeResource(
-                                    MainActivity.this.getResources(), R.drawable.custom_marker));
-
-                    //Retrieve current location from server
-                    originMarkerGeoJsonSource = new GeoJsonSource("origin-source-id", Feature.fromGeometry(
-                            Point.fromLngLat(currentLongitute, currentLattitude)));
-                    style.addSource(originMarkerGeoJsonSource);
-
-                    originMarkerSymbolLayer = new SymbolLayer("originMarker-layer-id", "origin-source-id");
-                    originMarkerSymbolLayer.withProperties(
-                            PropertyFactory.iconImage("origin-marker-icon-id")
-                    );
-                    style.addLayer(originMarkerSymbolLayer.withProperties(
-                            iconAllowOverlap(true),
-                            iconIgnorePlacement(true)
-                    ));
-                    map = mapboxMap;
-                    startTimer();
-                }
+                originMarkerSymbolLayer = new SymbolLayer("originMarker-layer-id", "origin-source-id");
+                originMarkerSymbolLayer.withProperties(
+                        PropertyFactory.iconImage("origin-marker-icon-id")
+                );
+                style.addLayer(originMarkerSymbolLayer.withProperties(
+                        iconAllowOverlap(true),
+                        iconIgnorePlacement(true)
+                ));
+                map = mapboxMap;
+                startTimer();
+            }
 
             public void startTimer() {
-                //set a new Timer
                 timer = new Timer();
-
-                //initialize the TimerTask's job
                 initializeTimerTask();
-
-                //schedule the timer, after the first 5000ms the TimerTask will run every 10000ms
-                timer.schedule(timerTask, 5000, 5000); //
+                timer.schedule(timerTask, 5000, Constants.REFRESH_RATE_IN_SECONDS * 1000);
             }
 
             public void stoptimertask(View v) {
@@ -176,11 +165,9 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
                 timerTask = new TimerTask() {
                     public void run() {
-
-                        //use a handler to run a toast that shows the current timestamp
                         handler.post(new Runnable() {
                             public void run() {
-                                //Update player when timer task runs
+                                //Update player when timer task runs if the player has selected a route
                                 if (hasSetRoute) {
                                     socket.emit("getPlayerUpdate");
                                     updateMarkerPosition();
@@ -195,32 +182,22 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mapboxMap.addOnMapClickListener(new MapboxMap.OnMapClickListener() {
             @Override
             public boolean onMapClick(@NonNull LatLng point) {
-
                 hasSetRoute = true;
-
                 destinationLattitude = point.getLatitude();
                 destinationLongitute = point.getLongitude();
-
                 //Retrieve "origin" (current location) from server
                 origin = Point.fromLngLat(currentLongitute, currentLattitude);
-
                 //Send Destination Information to server
                 //When destination change is sent server should automatically change course
                 destination = Point.fromLngLat(destinationLongitute, destinationLattitude);
-
                 Style style = mapboxMap.getStyle();
                 initSource(style);
-
                 initLayers(style);
-
                 getRoute(style, origin, destination);
-
                 updateMarkerPosition();
-
                 return false;
             }
         });
-
     }
 
     private void initSource(@NonNull Style loadedMapStyle) {
@@ -250,8 +227,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         if (loadedMapStyle.getLayer(ROUTE_LAYER_ID) == null) {
             loadedMapStyle.addLayer(routeLayer);
         }
-        /*loadedMapStyle.addImage(RED_PIN_ICON_ID, BitmapUtils.getBitmapFromDrawable(
-                getResources().getDrawable(R.drawable.custom_marker)));*/
 
         if (loadedMapStyle.getLayer(ICON_LAYER_ID) == null) {
             loadedMapStyle.addLayer(new SymbolLayer(ICON_LAYER_ID, ICON_SOURCE_ID).withProperties(
@@ -276,8 +251,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         client.enqueueCall(new Callback<DirectionsResponse>() {
             @Override
             public void onResponse(Call<DirectionsResponse> call, Response<DirectionsResponse> response) {
-//                mapboxMap.setStyle(new Style.Builder().fromUrl("mapbox://styles/stripedwristbands/cjrs8ad75iwpe2so1sq19ayom"));
-//                Style style = mapboxMap.getStyle();
                 System.out.println(call.request().url().toString());
 
                 Timber.d("Response code: " + response.code());
@@ -289,7 +262,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                     return;
                 }
 
-                //Send Current ROute GeoJson file to server
+                //Send Current Route GeoJson file to server
                 currentRoute = response.body().routes().get(0);
                 socket.emit("setTravelRoute", currentRoute.geometry(), currentRoute.distance(), currentRoute.duration());
 
@@ -312,8 +285,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     private void createGeoJsonSource(@NonNull Style loadedMapStyle) {
-        //Instead of loading from assets folder, retrieve from server
-//        loadedMapStyle.addSource(new GeoJsonSource(loadJsonFromAsset("Tornado_Watch.geojson")));
         loadedMapStyle.addSource(new GeoJsonSource(GEOJSON_SOURCE_ID,
                 loadJsonFromAsset("Tornado_Watch.geojson")));
         /*CustomGeometrySource source = new CustomGeometrySource("geojson-source", (FeatureCollection.fromJson(loadJsonFromAsset("Tornado_Watch.geojson"))));
@@ -341,6 +312,77 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             ex.printStackTrace();
             return null;
         }
+    }
+
+    /*
+        Event fires when server emits the updatePlayer event
+        server emits in response to an emit from App side of
+        socket.emit("getPlayerUpdate");
+     */
+    private Emitter.Listener onUpdatePlayer = new Emitter.Listener() {
+        @Override
+        public void call(final Object... args) {
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        JSONObject data = (JSONObject) args[0];
+                        Point currentLocation;
+                        int score;
+                        double timeLeft;
+                        JSONArray latlong;
+                        try{
+                            latlong = data.getJSONArray("currentLocation");
+                            currentLocation = Point.fromLngLat(latlong.getDouble(0), latlong.getDouble(1));
+                            score = data.getInt("currentScore");
+                            timeLeft = data.getDouble("timeLeft");
+                            updateLocation(currentLocation);
+                            updateTimeLeft(timeLeft);
+                            updateScore(score);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+        }
+    };
+
+    private void updateTimeLeft(double timeLeft) {
+        TextView timeText = findViewById(R.id.textView_Time);
+        timeText.setText(Integer.toString((int) floor(timeLeft)) + " Seconds");
+    }
+
+    private void updateScore(int score) {
+        TextView scoreText = findViewById(R.id.textView_Score);
+        scoreText.setText(Integer.toString(score));
+    }
+
+    public void updateLocation(Point currentLocationFromServer) {
+        currentLongitute = currentLocationFromServer.longitude();
+        currentLattitude = currentLocationFromServer.latitude();
+        currentLocation = Point.fromLngLat(currentLongitute, currentLattitude);
+        System.out.println(currentLocation);
+    }
+
+    private void updateMarkerPosition() {
+        if (map.getStyle() != null) {
+            originMarkerGeoJsonSource = map.getStyle().getSourceAs("origin-source-id");
+            if (originMarkerGeoJsonSource != null) {
+                originMarkerGeoJsonSource.setGeoJson(FeatureCollection.fromFeature(
+                        Feature.fromGeometry(Point.fromLngLat(currentLocation.longitude(), currentLocation.latitude()))
+                ));
+            }
+        }
+    }
+
+    public void switchToLoginScreen(View view) {
+        LayoutInflater inflater = getLayoutInflater();
+        login = inflater.inflate(R.layout.login, null);
+        getWindow().addContentView(login, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT,
+                ViewGroup.LayoutParams.FILL_PARENT));
+    }
+
+    public void switchToMainScreen(View view) {
+        ((ViewGroup) login.getParent()).removeView(login);
     }
 
     @Override
@@ -383,83 +425,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         mapView.onSaveInstanceState(outState);
-    }
-
-
-    /*
-        Event fires when server emits the updatePlayer event
-        server emits in response to an emit from App side of
-        socket.emit("getPlayerUpdate");
-     */
-    private Emitter.Listener onUpdatePlayer = new Emitter.Listener() {
-        @Override
-        public void call(final Object... args) {
-
-                MainActivity.this.runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        JSONObject data = (JSONObject) args[0];
-                        Point currentLocation;
-                        int score;
-                        double timeLeft;
-                        JSONArray latlong;
-                        try{
-                            latlong = data.getJSONArray("currentLocation");
-                            currentLocation = Point.fromLngLat(latlong.getDouble(0), latlong.getDouble(1));
-                            score = data.getInt("currentScore");
-                            timeLeft = data.getDouble("timeLeft");
-                            // call required method for updating icon location and pass it currentLocation
-                            updateLocation(currentLocation);
-                            // update score display once it is implemented
-                            updateTimeLeft(timeLeft);
-                            updateScore(score);
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                });
-        }
-
-    };
-
-    private void updateTimeLeft(double timeLeft) {
-        TextView timeText = findViewById(R.id.textView_Time);
-        timeText.setText(Integer.toString((int) floor(timeLeft)) + " Seconds");
-    }
-
-    private void updateScore(int score) {
-        TextView scoreText = findViewById(R.id.textView_Score);
-        scoreText.setText(Integer.toString(score));
-    }
-
-    public void updateLocation(Point currentLocationFromServer) {
-        currentLongitute = currentLocationFromServer.longitude();
-        currentLattitude = currentLocationFromServer.latitude();
-        currentLocation = Point.fromLngLat(currentLongitute, currentLattitude);
-        System.out.println(currentLocation);
-    }
-
-    private void updateMarkerPosition() {
-        if (map.getStyle() != null) {
-            originMarkerGeoJsonSource = map.getStyle().getSourceAs("origin-source-id");
-            if (originMarkerGeoJsonSource != null) {
-                originMarkerGeoJsonSource.setGeoJson(FeatureCollection.fromFeature(
-                        Feature.fromGeometry(Point.fromLngLat(currentLocation.longitude(), currentLocation.latitude()))
-                ));
-            }
-        }
-    }
-
-    public void switchToLoginScreen(View view) {
-        LayoutInflater inflater = getLayoutInflater();
-        login = inflater.inflate(R.layout.login, null);
-        getWindow().addContentView(login, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT,
-                ViewGroup.LayoutParams.FILL_PARENT));
-
-    }
-
-    public void switchToMainScreen(View view) {
-        ((ViewGroup) login.getParent()).removeView(login);
     }
 
 }
