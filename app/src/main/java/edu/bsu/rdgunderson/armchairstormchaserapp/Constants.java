722f1921bd701f8878cbaaf58cbd04043f1396bf
Robert <rdgunderson@bsu.edu>
2019-04-19 19:09:28 -0400

    Stop Travel Button

--- app/src/main/java/edu/bsu/rdgunderson/armchairstormchaserapp/Constants.java
+++ app/src/main/java/edu/bsu/rdgunderson/armchairstormchaserapp/Constants.java
@@ -3,8 +3,6 @@ package edu.bsu.rdgunderson.armchairstormchaserapp;
 public class Constants {
     public static final String SERVER_URL = "http://10.228.20.238:3000"; //ip of my laptop
     public static final String MAPBOX_API_KEY = "pk.eyJ1Ijoic3RyaXBlZHdyaXN0YmFuZHMiLCJhIjoiY2pvN3VrYWx6MDJsZjN3dGt1bDNjd2c0aiJ9.qeI4-uMxyL5JnEiPi3UVSQ";
-//    public static final String MAPBOX_STYLE_URL = "mapbox://styles/stripedwristbands/cjrs8ad75iwpe2so1sq19ayom";
-//    public static final String MAPBOX_STYLE_URL = "mapbox://styles/stripedwristbands/cjojc7pow0bjv2st5rkoinjza";
-    public static final String MAPBOX_STYLE_URL = "mapbox://styles/stripedwristbands/cju0hv17t0e4t1fmci3tbgihm";
+    public static final String MAPBOX_STYLE_URL = "mapbox://styles/stripedwristbands/cjuonea47c1dh1flpaocgkpqn";
     public static final int REFRESH_RATE_IN_SECONDS = 5;
 }
